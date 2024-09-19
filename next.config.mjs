48d2ef7e45a6c9b3901a934cc6fe304ac2ed25fe
Jasmehar Singh <jasmeharsingh1708@gmail.com>
2024-09-19 15:23:40 +0530

    initial commit

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['img.clerk.com'], // Add the external hostname here
+    },
+  };
+  
+  export default nextConfig;
+  
